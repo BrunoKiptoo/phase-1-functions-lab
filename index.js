309a9d405df037ce9b07527b7bd81e05c789c459
Bruno <brunochelimo@gmail.com>
2022-09-07 12:17:54 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,37 +1,43 @@
 // Code your solution in this file!
-function distanceFromHqInBlocks(someValue) {
-    if (someValue < 42){
-        return (42-someValue)
-    }
-    else {
-        return (someValue-42)
-    }
-  }
 
-  function distanceFromHqInFeet(someValue) {
-    return distanceFromHqInBlocks(someValue) * 264;
-  }
+function distanceFromHqInBlocks(blocks) {
+    if (blocks < 42) {   
+        return 42 - blocks;
+    } 
+    else  
+     {
+        return blocks - 42;
+    } 
+}
 
-  function distanceTravelledInFeet(start, destination) {
-    if (start<destination){
-        return ((destination - start) * 264);
-    }
-    else {
-        return((start-destination)*264);
-    }
-  }
-  function calculatesFarePrice(start, destination) {
-    let distance = distanceTravelledInFeet(start, destination);
-    if (distance<=400){
-        return 0;
-    }
-     if (distance >= 400 && distance <=2000) {
-         return (distance - 400)* 0.02;
-    }
-    else if (distance >=2000 && distance <=2500){
-        return 25;
-    }
-    else if (distance > 2500 ){
-        return 'cannot travel that far';
+
+
+ function distanceFromHqInFeet (blocks) {
+     return distanceFromHqInBlocks(blocks) * 264;
+ }
+
+
+
+function distanceTravelledInFeet(start,stop) {
+   if (stop > start) {
+       return (stop - start) * 264
+   } 
+   else 
+   {
+      return (start - stop) * 264
+   }
+}
+
+
+function calculatesFarePrice(start, stop){
+    const feetTravelled = distanceTravelledInFeet(start,stop);
+    if(feetTravelled<=400){
+      return 0;
+    }else if(feetTravelled>400 && feetTravelled<=2000){
+      return (feetTravelled-400) * 0.02;
+    }else if(feetTravelled>2000 && feetTravelled<=2500){
+      return 25;
+    }else if(feetTravelled>2500){
+      return "cannot travel that far";
     }
   }
